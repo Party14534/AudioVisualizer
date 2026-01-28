e67317cb203897700591229fe6f8430552425b80
Zachariah Dellimore <42615901+Party14534@users.noreply.github.com>
2026-01-27 20:52:23 -0500

    Can play audio and has a bad visualizer:

--- src/AudioTypes/WAV/wav.cpp
+++ src/AudioTypes/WAV/wav.cpp
@@ -1,4 +1,5 @@
 #include "wav.h"
+#include <SFML/Audio/SoundBuffer.hpp>
 #include <cstdio>
 
 AudioFile* getWav(std::string pathToFile) {
@@ -49,9 +50,99 @@ AudioFile* getWav(std::string pathToFile) {
         exit(1);
     }
 
-    u16 format = (h.AudioFormat[1] << 8) | h.AudioFormat[0];
+    // Read sampled data
+    char dataId[5] = "data";
+    int dataIndex = 0;
+    long dataStart;
+    for(;;) {
+        // TODO: do something with metadata
+        byte character;
+        size_t bytesRead = fread(&character, 1, 1, f);
+        if(bytesRead != 1) {
+            printf("Invalid WAV file\nError in DataBlocID\n");
+            exit(1);
+        }
+        
+        // Validate Data Bloc ID
+        if (character == dataId[dataIndex]) {
+            if (character == 'd') { dataStart = ftell(f); }
+            dataIndex++;
 
-    printf("%d\n", format);
+            if (dataIndex == 4) { break; }
+        }
+    }
+
+    fread(&h.DataSize, 4, 1, f);
+
+    u16 format = (h.AudioFormat[1] << 8) | h.AudioFormat[0];
+    /*u32 dataSize = (h.DataSize[0]) | (h.DataSize[1] << 8) |
+        (h.DataSize[2] << 16) | (h.DataSize[3] << 24);*/
+
+    switch (format) {
+        case 0x01:
+            h.PCMData = (i16*)malloc(h.DataSize);
+            // TODO: make this faster
+            for (int i = 0; i < h.DataSize; i+=2) {
+                byte lsb, msb;
+                fread(&lsb, 1, 1, f);
+                fread(&msb, 1, 1, f);
+                h.PCMData[i/2] = (msb << 8) | lsb;
+            }
+            break;
+        case 0x03:
+            printf("Unsupported WAVE Format\n");
+            exit(1);
+            break;
+        default:
+            printf("Unknown WAVE Format\n");
+            exit(1);
+            break;
+    }
+
+    // Create appropriate channel map
+    std::vector<sf::SoundChannel> channelMap;
+
+    printf("%d\n", h.NbrChannels);
+
+    if (h.NbrChannels == 1) {
+        channelMap = {sf::SoundChannel::Mono};
+    } else if (h.NbrChannels == 2) {
+        // Standard stereo: Left then Right
+        channelMap = {sf::SoundChannel::SideLeft, sf::SoundChannel::SideRight};
+    } else if (h.NbrChannels == 4) {
+        // Quadraphonic (less common)
+        channelMap = {
+            sf::SoundChannel::FrontLeft,
+            sf::SoundChannel::FrontRight,
+            sf::SoundChannel::BackLeft,
+            sf::SoundChannel::BackRight
+        };
+    } else if (h.NbrChannels == 6) {
+        // 5.1 surround
+        channelMap = {
+            sf::SoundChannel::FrontLeft,
+            sf::SoundChannel::FrontRight,
+            sf::SoundChannel::FrontCenter,
+            sf::SoundChannel::LowFrequencyEffects,
+            sf::SoundChannel::SideLeft,
+            sf::SoundChannel::SideRight
+        };
+    } else {
+        // Fallback: assign channels sequentially
+        for (unsigned int i = 0; i < h.NbrChannels; ++i) {
+            channelMap.push_back(static_cast<sf::SoundChannel>(i));
+        }
+    }
+
+    // Set up AudioFile
+    audioFile->buffer = sf::SoundBuffer(h.PCMData,
+            h.DataSize / sizeof(i16),
+            h.NbrChannels,
+            h.Frequency,
+            channelMap);
+    audioFile->Frequency = h.Frequency;
+    audioFile->channelCount = h.NbrChannels;
+    audioFile->sampleCount = h.DataSize / sizeof(i16);
 
     fclose(f);
     return audioFile;
--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,38 @@
 #include "main.h"
+#include <SFML/Graphics/RectangleShape.hpp>
+#include <cstdlib>
 
 int main() {
     sf::RenderWindow win(sf::VideoMode::getDesktopMode(), "Audio Visualizer", sf::Style::Default);
 
     struct AudioFile* a = getWav("../files/suck.wav");
+    sf::Sound sound(a->buffer);
+    sound.play();
 
-    /*
+    sf::RectangleShape bar;
+    bar.setSize({50.f, 0.f});
+    bar.setOrigin({25.f, 0.f});
+    bar.setFillColor(sf::Color::Green);
+    bar.setPosition({(float)win.getSize().x * 0.5f, (float)win.getSize().y});
+
+    const i16* buff = a->buffer.getSamples();
+
     while (win.isOpen()) {
         // Event handling
         handleEvents(win);
 
+        float t = sound.getPlayingOffset().asSeconds();
+        int index = t * a->channelCount * a->Frequency;
+        float height = -(float(abs(buff[index])) / float(INT16_MAX)) * 500.f;
+        bar.setSize({50.f, height});
+
         // Render handling
-        handleRendering(win);
-    }*/
+        win.clear(sf::Color(230,215,255));
+
+        win.draw(bar);
+        
+        win.display();
+    }
 
     printf("Successfully parsed file\n");
 
